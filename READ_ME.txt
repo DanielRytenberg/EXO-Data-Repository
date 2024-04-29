515b2c964e2d2c75882293befffaa6b1223e4a63
CoffeElliot <elliot.b00din@gmail.com>
2024-04-29 15:38:15 +0200

    Script for saving data

---
+++ READ_ME.txt
@@ -0,0 +1,11 @@
+The rows 7-14 are the variables you could change and the script still runes.
+
+To run the scrip:
+	First open Wavegen and Scope
+	Change the varaibels as you please on the rows 7 - 14
+	Run the script for a few seconds and then press "Abort". This is because the Scope takes two runns to fully change it's variables. Idk why but it is what it is.
+	Afterwords run the script.
+
+Notes:
+	Let the file "DataIndex.txt" start with 100 and not 001. This is because the script will be smart and se the file as integers and not as a text file. This leas all the 
+	zeros before an value is removed.
---
+++ Script_java.js
@@ -0,0 +1,132 @@
+// This script adjusts the Wavegen offset based on Scope measurement.
+clear(); // clear log
+if(!('Wavegen' in this) || !('Scope' in this)) throw "Please open a Scope and a Wavegen instrument"; // open scope & wave
+
+
+//// CHANGE THESE VALUES!!!!!  ################
+var high = 5;
+var low = 0;
+var fs = 4000;
+var sec = 30;
+
+var TimeLed = 3; //Sec
+var incrementLed = 0;
+var MaxClasses = 3; // index modolo(MaxClasses) = {0 = resting, 1 = upp, 2 = down}
+//#######################################
+
+// setting all variables for script
+var time = new Date();
+var date = time.getUTCFullYear()+"_"+time.getUTCMonth()+"_"+time.getUTCDate();
+print(date); 
+var TestSubjectID =  + Tool.getText("TestSubject ID","ID");
+var DataPath = "C:/Users/Elliot/Desktop/åk3/EXO2/Data/DataIndex.txt";
+var index = File(DataPath).read(3);
+var path = "C:/Users/Elliot/Desktop/åk3/EXO2/Data/"+date+"_TestSub"+TestSubjectID+"_"+index+".csv";
+var file = File(path);
+var text = "TID;AMP Channel 1;AMP Channel 2;CLASS\n";
+var remaining = 0;
+var lable = 0;
+
+
+// opening and setting everything for Wavegen
+Wavegen.run();
+Wavegen.Channel1.Simple.Type.value = "DC";
+Wavegen.Channel1.Mode.text = "Simple";
+Wavegen.Channel1.Simple.Offset.value = 0;
+
+Wavegen.Channel2.Simple.Type.value = "DC";
+Wavegen.Channel2.Mode.text = "Simple";
+Wavegen.Channel2.Simple.Offset.value = 0;
+
+
+// opening and setting everything for Scope1
+Scope.run();
+Scope.Trigger.text = "Repeated";
+Scope1.Time.Position.value = 0;
+Scope1.Time.Rate.value = fs;
+Scope1.Time.Samples.value= fs*sec;//number of samples
+Scope1.Time.Mode.text= "Record";//record setting of the scope so that the values can be saved
+
+
+ Wavegen.Channel1.Simple.Offset.value = low;
+ Wavegen.Channel2.Simple.Offset.value = low;
+
+ if(!wait(1)) throw "Canceled" // for time to config 
+
+for(var i = 0; i < round(sec/TimeLed); i++){
+
+ 
+      remaining = i%MaxClasses;
+      if(remaining == 1)
+      {
+        lable = 1;
+             Wavegen.Channel1.Simple.Offset.value = high;
+             Wavegen.Channel2.Simple.Offset.value = low;
+      }
+      else if(remaining == 2)
+      {
+        lable = 2;
+             Wavegen.Channel1.Simple.Offset.value = low;
+             Wavegen.Channel2.Simple.Offset.value = high;
+      }
+      else if(remaining == 3)
+      {
+        lable = 3;
+             Wavegen.Channel1.Simple.Offset.value = high;
+             Wavegen.Channel2.Simple.Offset.value = high;
+      }
+      else
+      {
+        lable = 0;
+             Wavegen.Channel1.Simple.Offset.value = low;
+             Wavegen.Channel2.Simple.Offset.value = low;
+      }
+    if(!wait(TimeLed)) throw "Canceled"
+}
+
+var d1 = Scope.Channel1.data;
+var d2 = Scope.Channel2.data;
+
+const d = [d1,d2];
+
+//---------------printing and saving data
+var t = 1/fs;
+var i = 1;
+d1.forEach(function(sample)
+{
+    i++
+    t = i/fs;
+
+  if(i % (TimeLed*fs)  == 0)
+    {
+      
+      incrementLed ++;
+      remaining = incrementLed%MaxClasses;
+      if(remaining == 1)
+      {
+        lable = 1;
+      }
+      else if(remaining == 2)
+      {
+        lable = 2;
+      }
+      else if(remaining == 3)
+      {
+        lable = 3;
+      }
+      else
+      {
+        lable = 0;
+      }
+    print(lable);
+    }
+
+    text = text + t+";"+ d2[i] +";" + sample+";" + lable+"\n";
+})
+ 
+File(path).writeLine(text);
+//incrementing for next file 
+index ++;
+File(DataPath).writeLine(index);
+Scope.stop();
+Wavegen.stop();
