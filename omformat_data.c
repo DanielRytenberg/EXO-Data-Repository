22c2eefb78d470821c5325e36bb616606f3bb046
DanRyten <daniel.rytenberg@gmail.com>
2024-05-15 15:06:51 +0200

    Fixed so it worked when theres impulse declared

--- omformat_data.c
+++ omformat_data.c
@@ -29,7 +29,7 @@ void insertIntToStr(char* str, int adressStart, int adressStop, int insert)
 
 FILE* pathFinder(int* months,int*  days, int currentPoint,char* location, char* openingType, char* saveLocation)
 {
-    printf("Start Pathfinder\n");
+    //printf("Start Pathfinder\n");
     FILE *file;
     char path[MAX_LINE_LENGTH];
     char temp[MAX_LINE_LENGTH];
@@ -91,6 +91,7 @@ FILE* pathFinder(int* months,int*  days, int currentPoint,char* location, char* 
 int main() {
     printf("Start\n");
 
+
     char saveLocation[MAX_LINE_LENGTH];
     int days[2];
     days[0] = 0;
@@ -98,132 +99,148 @@ int main() {
     int months[2];
     months[0] = 4;
     months[1] = 4;
-    FILE *file = pathFinder(months,days,106, "", "r+", saveLocation);
-    if (file == NULL) {
-        perror("Error opening file");
-        return 1;
-    }
-    else
-    {
-        printf("Found file\n");
-    }
-
-    printf(saveLocation);
-    printf("\n");
-    FILE *save = fopen("sav", "w");
-    if (save == NULL) {
-        perror("Error opening file");
-        return 1;
-    }
-    else
-    {
-        printf("Found save location\n");
-    }
-    printf("Start\n");
-
     char line[MAX_LINE_LENGTH];
     char new_label[MAX_LINE_LENGTH];
     char newline[MAX_LINE_LENGTH];
     char startLines[MAX_LINE_LENGTH];
-    int sec,lable,lableFrequency = 0,i = 0,fs = 4000;
+    int sec,lable,lableFrequency,i = 0,fs = 4000;
     char *direction;
-
-    printf("variables made\n");
-
-    // skip 5 first lines and save sec and direction
-    for (int skip = 0; skip < 5; skip++) {
-        fgets(line, sizeof(line), file);
-        if(skip == 1)
-        {
-            sscanf(&line[18], "%c", &direction);
-            //printf("Direction is:%c\n",direction);
-        }
-        if(skip == 3)
-        {
-            sscanf(&line[15], "%d", &sec);
-            //printf("Sec is:%d\n",sec);
-            lableFrequency = sec*fs;
-        }
-        strcat(startLines,line);
-        printf("%s",line);
-    }
-    //printf("LableFreq is:%d\n", lableFrequency);
-    printf(startLines);
-
-    //Save the first lines to the doccument
-    fprintf(save,startLines);
-
-    //Chose starting lable based on diraction
-    if(*direction == 'u' || *direction == 'U')
+    for(int currentPoint = 228; currentPoint <= 260; currentPoint++)
     {
+
+        strcpy(line,"");
+        strcpy(new_label,"");
+        strcpy(newline,"");
+        strcpy(startLines,"");
+        i = 0;
+        sec = 0;
         lable = 0;
-    }
-    else if(*direction == 'd' || *direction == 'D')
-    {
-        lable = 2;
-    }
-    //printf("lable = %d", lable);
+        lableFrequency = 0;
+
+        FILE *file = pathFinder(months,days,currentPoint, "", "r+", saveLocation);
+        if (file == NULL) {
+            perror("Error opening file");
+            return 1;
+        }
+        else
+        {
+            printf("Found file of %d\n", currentPoint);
+        }
+
+        //printf(saveLocation);
+        printf("\n");
+        FILE *save = fopen(saveLocation, "w");
+        if (save == NULL) {
+            perror("Error opening file");
+            return 1;
+        }
+        else
+        {
+            //printf("Found save location\n");
+        }
+        //printf("Start\n");
+
+        
 
 
-    // Read file line by line
-    while (fgets(line, sizeof(line), file)) {
-           // Find the length of the string
-
-            int length = 0;
-            while (line[length] != '\n') {
-                if(length != 0)
-                    newline[length-1] = line[length-1];
-                length++;
-            }
-            newline[length-1] = '\0';
-            
-            /*
-            if(length > 0)
+        // skip 5 first lines and save sec and direction
+        for (int skip = 0; skip < 5; skip++) {
+            fgets(line, sizeof(line), file);
+            if(skip == 1)
             {
-                printf("prev char:%s", line);
-                printf("test char:%s\n",newline);
+                sscanf(&line[18], "%c", &direction);
+                //printf("Direction is:%c\n",direction);
             }
-            */
+            if(skip == 3)
+            {
+                sscanf(&line[15], "%d", &sec);
+                //printf("Sec is:%d\n",sec);
+                lableFrequency = sec*fs;
+            }
 
-            sprintf(new_label, "%d", lable);
-            strcat(newline,new_label);
-            strcat(newline,"\n");
-           // strcat(newline,"\0"); 
-           // printf("%s\n",new_label);
+            strcat(startLines,line);
+            //printf(line);
+            //printf(startLines);
+            //printf("\n");
+        }
+        printf("LableFreq is:%d\n", lableFrequency);
+        printf(startLines);
+        printf("\n");
 
-             // print the last character
-            if (length > 0) {
-                fprintf(save,"%s",newline);
-                //printf("last char:%s\n\n",newline);
-                //printf("last char:%c\n",newline[length]);
-                if(i%500 == 0)
-                {
-                    //printf("last char:%s\n",newline);
+        //Save the first lines to the doccument
+        fprintf(save,startLines);
+
+        //Chose starting lable based on diraction
+        if(*direction == 'u' || *direction == 'U')
+        {
+            lable = 0;
+        }
+        else if(*direction == 'd' || *direction == 'D')
+        {
+            lable = 2;
+        }
+        //printf("lable = %d\n", lable);
+
+
+        // Read file line by line
+        while (fgets(line, sizeof(line), file)) {
+            // Find the length of the string
+
+                int length = 0;
+                while (line[length] != '\n') {
+                    if(length != 0)
+                        newline[length-1] = line[length-1];
+                    length++;
                 }
-            }
+                newline[length-1] = '\0';
+                
+                /*
+                if(length > 0)
+                {
+                    printf("prev char:%s", line);
+                    printf("test char:%s\n",newline);
+                }
+                */
+
+                sprintf(new_label, "%d", lable);
+                strcat(newline,new_label);
+                strcat(newline,"\n");
+            // strcat(newline,"\0"); 
+            // printf("%s\n",new_label);
+
+                // print the last character
+                if (length > 0) {
+                    fprintf(save,"%s",newline);
+                    //printf("last char:%s\n\n",newline);
+                    //printf("last char:%c\n",newline[length]);
+                    if(i%500 == 0)
+                    {
+                        //printf("last char:%s\n",newline);
+                    }
+                }
 
 
-            i++;
-            /*if(i > 100)
-            {
-                break;
-            }*/
-            
-            // labeling
-            if(i%(lableFrequency) == 0)
-            {
-                lable = (1+lable)%4;
-                //printf("Lable is now %d\n", lable);
-            }
+                i++;
+                /*if(i > 100)
+                {
+                    break;
+                }*/
+                
+                // labeling
+                if(i%(lableFrequency) == 0)
+                {
+                    lable = (1+lable)%4;
+                    //printf("Lable is now %d\n", lable);
+                }
 
 
+        }
+        //printf("End\n");
+
+        fclose(file);
+        
+        fclose(save);
     }
-    printf("End\n");
-
-    fclose(file);
-    
-    fclose(save);
-
     return 0;
 }
 
